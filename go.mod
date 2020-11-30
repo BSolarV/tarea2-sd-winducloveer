de7434bf510e8fd281e0933fdac687e432f9b319
windu2 <carlos.jaraa@sansano.usm.cl>
2020-11-30 14:07:30 -0300

    editing namenode

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/BSolarV/tarea2-sd-winducloveer
 go 1.15
 
 require (
+	github.com/golang/protobuf v1.4.3 // indirect
 	google.golang.org/grpc v1.33.2
 	google.golang.org/protobuf v1.25.0
 )
--- namenode/namenode.go
+++ namenode/namenode.go
@@ -10,13 +10,17 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
+
+	"github.com/BSolarV/tarea2-sd-winducloveer/protoName"
+	protoNode "github.com/BSolarV/tarea2-sd-winducloveer/protoName"
 
 	"google.golang.org/grpc"
 )
 
 /*
 Definitivamente no compila! c:
-No tenia el proto y no sabia que más hacer C: */
+No tenia el protoNode y no sabia que más hacer C: */
 
 const ipDatanode0 = "localhost"
 const ipDatanode1 = "localhost"
@@ -66,6 +70,7 @@ func main() {
 	grpcServer := grpc.NewServer()
 
 	// ProtoLogistic.RegisterProtoLogisticServiceServer(grpcServer, srv)
+	protoName.RegisterProtoServiceServer(grpcServer, srv)
 
 	// Montando servidor GRPC
 	if err := grpcServer.Serve(lis); err != nil {
@@ -74,6 +79,7 @@ func main() {
 
 }
 
+//Book es la estructura de Libro
 type Book struct {
 	id            string
 	bookname      string
@@ -82,70 +88,78 @@ type Book struct {
 	timestamp     string
 }
 
+//NameNode es el Server
 type NameNode struct {
 
 	//el log es el registro de los libros
 	log []Book
 
-	writequeue 
 	// Para bloquear recursos entre hilos
 	mutex sync.Mutex
 }
 
 //NewServer es el constructor del Server
-func newNameNode() *Server {
-	var srv Server
+func newNameNode() *NameNode {
+	var srv NameNode
 
 	return &srv
 }
 
-//ReadRequest es invocada por el cliente para leer el log
-func (s *Server) ReadRequest(ctx context.Context, request *proto.ReadRequest) (*proto.LogData, error) {
-	mutex.Lock()
-	var paq *proto.LogData
-	for _, book := range log {
-		aux := ""
-		if book.bookname == request.bookname {
-			paq.id = book.id
-			paq.timestamp = book.timestamp
-			msg := book.bookname + ";" + book.partsNum + ";"
+//ClientRequest es invocada por el cliente para leer el log
+func (s *NameNode) ClientRequest(ctx context.Context, request *protoNode.ReadRequest) (*protoNode.LogData, error) {
+	s.mutex.Lock()
+	var paq *protoNode.LogData
+	aux := ""
+	msg := ""
+	for _, book := range s.log {
+		if book.bookname == request.GetBookname() {
+			paq.Id = book.id
+			paq.Timestamp = book.timestamp
+			msg += book.bookname + ";" + book.partsNum + ";"
 			for _, part := range book.partsLocation {
 				aux += part + ","
 			}
 		}
 	}
-	mutex.Unlock()
+	s.mutex.Unlock()
 	msg += aux
 
-	paq.message = msg[:-1]
+	paq.Message = msg[:len(msg)-1]
 
 	return paq, nil
 }
 
-//esta función es innecesaria al parecer
-func (s *Server) WriteRequest(ctx context.Context, request *proto.EditRequest) (*proto.Response, error) {
-	return nil,nil
+//WriteRequest es innecesaria al parecer
+func (s *NameNode) WriteRequest(ctx context.Context, request *protoNode.WriteRequest) (*protoNode.Response, error) {
+	return nil, nil
 }
 
-//cuando el nodo reune permisos escribe con esta funcion
-func (s *Server) WriteLog(ctx context.Context, packageToWrite *proto.LogData) (*proto.Empty, error) {
-	mutex.Lock()
+//WriteLog se usa cuando el nodo reune permisos y escribe con esta funcion
+func (s *NameNode) WriteLog(ctx context.Context, packageToWrite *protoNode.LogData) (*protoNode.Empty, error) {
+	s.mutex.Lock()
 	var book Book
 
-	aux := packageToWrite.Getmessage()
-	aux = strings.Split(aux, ";")
-	aux2 := strings.Split(aux[2], ",")
+	aux := packageToWrite.GetMessage()
+	aux1 := strings.Split(aux, ";")
+	aux2 := strings.Split(aux1[2], ",")
 
-	book.id = packageToWrite.Getid()
-	book.bookname = aux[0]
-	book.partsNum = aux[1]
+	book.id = packageToWrite.GetId()
+	book.bookname = aux1[0]
+	book.partsNum = aux1[1]
 	book.partsLocation = aux2
-	book.timestamp = //quien crea el timestamp??
+	book.timestamp = time.Now().Format("2006.01.02 15:04:05")
 
-	log = append(log, book)
+	s.log = append(s.log, book)
 
-	mutex.Unlock()
+	s.mutex.Unlock()
 
-	return  proto.Empty{}, nil
+	return &protoNode.Empty{}, nil
 
+}
+
+//CheckProposal chequea propuestas
+func (s *NameNode) CheckProposal(ctx context.Context, proposal *protoNode.Proposal) (*protoNode.Response, error) {
+	res := true
+
+	return &protoNode.Response{Timestamp: time.Now().Unix(), Response: res}, nil
 }
