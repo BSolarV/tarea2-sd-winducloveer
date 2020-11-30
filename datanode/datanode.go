dbd0be086c854d8f80e59ef98ced21f7ccab6b63
Bastián Alejandro Solar Vargas <bsolar@alumnos.inf.utfsm.cl>
2020-11-30 05:11:11 -0300

    Intento de proposal

--- datanode/datanode.go
+++ datanode/datanode.go
@@ -5,11 +5,13 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
 	"os"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	protoNode "github.com/BSolarV/tarea2-sd-winducloveer/protoNode"
 
@@ -63,7 +65,7 @@ func main() {
 	defer lis.Close()
 
 	// Creando instancia del nodo
-	srv := newDataNode()
+	srv := newDataNode(index)
 	// creando instancia de servidor GRPC
 	grpcServer := grpc.NewServer()
 
@@ -84,29 +86,33 @@ func main() {
 //	checkProporsal(Proporsal) returns (Response) {}
 
 type DataNode struct {
-	proposalQueue      []string
-	writeProposalQueue []string
+	index          int
+	availableSpace int
+
+	proposalQueue        []string
+	writePermisionsQueue []string
 
 	// Para bloquear recursos entre hilos
 	mutex sync.Mutex
 }
 
 //newDataNode es el constructor del Server
-func newDataNode() *DataNode {
+func newDataNode(actualIndex int) *DataNode {
 	var srv DataNode
-
+	srv.index = actualIndex
+	srv.availableSpace = 1024 // espacio para chunks, 256 MegaBytes aproximadamente
 	return &srv
 }
 
-//Usaremos bully algorithm de https://moodle.inf.utfsm.cl/pluginfile.php/104700/mod_resource/content/0/Chapter%204_clase3.pdf
-func (*DataNode) BuildProposal() (*protoNode.Proporsal, error) {
-	return &protoNode.Proporsal{}, nil
-}
-
 //checkProporsal: Coordinación entre DataNodes y entre NameNode con DataNode para verificar la propuesta
-func (*DataNode) CheckProporsal(ctx context.Context, proporsal *protoNode.Proporsal) (*protoNode.Response, error) {
-	response := &protoNode.Response{}
-	return response, nil
+func (srv *DataNode) CheckProposal(ctx context.Context, proposal *protoNode.Proposal) (*protoNode.Response, error) {
+	res := true
+	srv.mutex.Lock()
+	if srv.availableSpace-int(proposal.GetNumChunks()) < 0 {
+		res = false
+	}
+	srv.mutex.Unlock()
+	return &protoNode.Response{Timestamp: time.Now().Unix(), Response: res}, nil
 }
 
 //updateLog: Escritura de DataNode en NameNode
@@ -121,12 +127,180 @@ func (*DataNode) WritePermisions(ctx context.Context, writeRequest *protoNode.Wr
 	return response, nil
 }
 
-//sendChunk: Envia el chunk a guardar
-func (*DataNode) RecieveChunk(ctx context.Context, chunkPackage *protoNode.ChunkPackage) (*protoNode.Empty, error) {
+//RecieveChunks: Envia el chunk a guardar
+func (*DataNode) RecieveChunks(ctx context.Context, chunksPackage *protoNode.ChunksPackage) (*protoNode.Empty, error) {
+	for _, oneChunk := range chunksPackage.Chunks {
+		fmt.Printf("Recieved part %d of book %s.\n", oneChunk.GetNumChunkActual(), chunksPackage.GetBookName())
+	}
 	return &protoNode.Empty{}, nil
 }
 
-func (*DataNode) UploadFile(ctx context.Context, splittedFile *protoNode.SplittedFile) (*protoNode.Empty, error) {
-	fmt.Printf("%s: %x", splittedFile.Name, splittedFile.Chunks[:5][:5])
+type Proposal struct {
+	dict map[int][]int // mapa de indice DataNode a indices de Chunks a enviar a dicho dataNode
+}
+
+// Usaremos bully algorithm de https://moodle.inf.utfsm.cl/pluginfile.php/104700/mod_resource/content/0/Chapter%204_clase3.pdf
+// Creo que no hace falta el bully, mejor random, porque no se puede pasar el mando, la propuesta y envio de chunks debe hacerlo el mismo datanode que los recibio
+func (srv *DataNode) BuildProposal(validIndexes []int, services []protoNode.ProtoServiceClient, numOfChunks int) (Proposal, error) {
+	proposal := Proposal{dict: make(map[int][]int)}
+	var index int
+	var availableDataNodes []int
+	var dataNodeProposal *protoNode.Proposal
+	var response *protoNode.Response
+	var err error
+
+	flag := false
+	for flag == false {
+
+		flag = true
+
+		availableDataNodes = validIndexes
+		for i := 0; i < numOfChunks; i++ {
+			if len(availableDataNodes) == 0 {
+				availableDataNodes = validIndexes
+			}
+			index = rand.Intn(len(availableDataNodes))
+			fmt.Printf("Picked %d\n", index)
+			availableDataNodes[index] = availableDataNodes[len(availableDataNodes)-1]
+			availableDataNodes[len(availableDataNodes)-1] = 0
+			availableDataNodes = availableDataNodes[:len(availableDataNodes)-1]
+
+			fmt.Printf("Pickeeed %d\n", index)
+			proposal.dict[index] = append(proposal.dict[index], i)
+		}
+		fmt.Println("++++++++++++++++++++++++++++")
+		for key, value := range proposal.dict {
+			fmt.Printf("	datanode %d\n", key)
+			for i := 0; i < len(value); i++ {
+				fmt.Printf("		parte %d\n", value[i])
+			}
+		}
+		fmt.Println("++++++++++++++++++++++++++++")
+		var dataNodeService protoNode.ProtoServiceClient
+		for key, value := range proposal.dict {
+			dataNodeProposal = &protoNode.Proposal{Node: int64(srv.index), NumChunks: int64(len(value)), Timestamp: time.Now().Unix()}
+			if key == srv.index {
+				response, err = srv.CheckProposal(context.Background(), dataNodeProposal)
+			} else {
+				dataNodeService = services[key]
+				response, err = dataNodeService.CheckProposal(context.Background(), dataNodeProposal)
+			}
+			if err != nil || !response.Response {
+				validIndexes[key] = validIndexes[len(validIndexes)-1]
+				validIndexes[len(validIndexes)-1] = 0
+				validIndexes = validIndexes[:len(validIndexes)-1]
+				flag = false
+				continue
+			}
+		}
+	}
+	return proposal, nil
+}
+
+// El plan era establecer conexion y mantenerla hasta enviar los paquetes, pero la forma para guardar los servicios no esta funcionando.
+// Usar punteros a interfaces por algun motivo es pesima idea segun stackoverflow
+// El otro plan sería guardar los servicios como un atributo del server.
+// El otro plan sería guardar solamente las conexiones, montar los servicios a medida que se requieran.
+func (srv *DataNode) UploadFile(ctx context.Context, splittedFile *protoNode.SplittedFile) (*protoNode.Empty, error) {
+	fmt.Printf("%s: %x\n", splittedFile.Name, len(splittedFile.Chunks))
+
+	var validIndexes []int
+	var connections []*grpc.ClientConn
+	var services []protoNode.ProtoServiceClient
+
+	// Estableciendo conexiones para proposal y enviar chunks
+	var ip, port string
+	var conn *grpc.ClientConn
+	var dataNodeService protoNode.ProtoServiceClient
+	var err error
+	for i := 0; i < 3; i++ {
+		if i == srv.index {
+			validIndexes = append(validIndexes, i)
+			connections = append(connections, nil)
+			services = append(services, nil)
+			continue
+		}
+		switch i {
+		case 0:
+			ip = ipDatanode0
+			port = "9000"
+		case 1:
+			ip = ipDatanode1
+			port = "9001"
+		case 2:
+			ip = ipDatanode2
+			port = "9002"
+		}
+
+		conn, err = grpc.Dial(ip+":"+port, grpc.WithInsecure())
+		if err != nil {
+			// No se añade su indice como uno valido
+			connections = append(connections, nil)
+			services = append(services, nil)
+			continue
+		}
+
+		validIndexes = append(validIndexes, i)
+		connections = append(connections, conn)
+		dataNodeService = protoNode.NewProtoServiceClient(conn)
+		services = append(services, dataNodeService)
+	}
+
+	fmt.Println("-----------------------")
+	for _, service := range services {
+		asdf, fdsa := service.PrintIndex(context.Background(), &protoNode.Empty{})
+		if fdsa != nil {
+			panic(fdsa)
+		}
+		fmt.Printf("Recieved: %s\n", asdf.Id)
+	}
+	fmt.Println("-----------------------")
+
+	var proposal Proposal
+
+	var chunks []*protoNode.Chunk
+	var chunksToSend *protoNode.ChunksPackage
+
+	flag := false
+	for flag == false {
+
+		flag = true
+
+		proposal, err = srv.BuildProposal(validIndexes, services, len(splittedFile.Chunks))
+		if err != nil {
+			flag = false
+			fmt.Printf("ERROR! %s\n", err)
+			continue
+		}
+
+		for key, value := range proposal.dict {
+
+			chunks = make([]*protoNode.Chunk, len(value))
+
+			for i := 0; i < len(value); i++ {
+				chunks = append(chunks, &protoNode.Chunk{FileName: splittedFile.Name, NumChunkActual: int64(i), Chunk: splittedFile.Chunks[value[i]]})
+			}
+			chunksToSend = &protoNode.ChunksPackage{BookName: splittedFile.Name, Chunks: chunks}
+
+			if key == srv.index {
+				_, err = srv.RecieveChunks(context.Background(), chunksToSend)
+			} else {
+				_, err = services[key].RecieveChunks(context.Background(), chunksToSend)
+			}
+
+			if err != nil {
+				flag = false
+				break
+			}
+		}
+	}
+	for _, connection := range connections {
+		connection.Close()
+	}
 	return &protoNode.Empty{}, nil
+}
+
+func (srv *DataNode) PrintIndex(ctx context.Context, _ *protoNode.Empty) (*protoNode.Response, error) {
+	fmt.Printf("Soy el indice %d\n", srv.index)
+	return &protoNode.Response{Id: strconv.Itoa(srv.index)}, nil
 }
